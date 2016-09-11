7e3edd48721444da2f1a374082f61c4a68bb7ff7
Efp95 <edsonflorespalma@gmail.com>
2016-09-10 22:37:17 -0500

    Adding Akka.Net Actor

---
+++ AkkaNinject/Actors/WordInspectorActor.cs
@@ -0,0 +1,54 @@
+﻿using Akka.Actor;
+using AkkaNinject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaNinject.Actors
+{
+    class WordInspectorActor : ReceiveActor
+    {
+        private readonly IPrinterService _printerService;
+
+        public WordInspectorActor(IPrinterService printerService)
+        {
+            _printerService = printerService;
+
+            Receive<string>(word =>
+            {
+                Inspect(word);
+            });
+        }
+
+        private void Inspect(string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                _printerService.Run(ValidationMessages.EmptyInput);
+
+                Self.Tell(Console.ReadLine());
+            }
+            else
+            {
+                if (message.Length % 2 == 0)
+                {
+                    _printerService.Run(ValidationMessages.ValidInput);
+                }
+                else
+                {
+                    _printerService.Run(ValidationMessages.InvalidInput);
+                }
+            }
+        }
+
+        struct ValidationMessages
+        {
+            public const string EmptyInput = "You must send a word";
+
+            public const string ValidInput = "You sent a valid input";
+            public const string InvalidInput = "You sent an invalid input";
+        }
+    }
+}
