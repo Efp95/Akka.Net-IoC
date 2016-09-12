68e4aaf3b01f05847fd035a269a4284879d9df2e
Efp95 <edsonflorespalma@gmail.com>
2016-09-12 17:49:48 -0500

    SimpleInjector implementation

--- AkkaNet.SimpleInjector/Program.cs
+++ AkkaNet.SimpleInjector/Program.cs
@@ -1,11 +1,26 @@
-﻿
+﻿using AkkaNet.Actors;
+using System;
+
 namespace AkkaNet.SimpleInjector
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            AkkaConfig.RegisterActors();
+            SimpleInjectorConfig.RegisterInjections();
+
+            var actorSystem = AkkaConfig.InspectorActorSystem;
+            var wordInspectorActor = actorSystem.ActorOf(
+                                        SimpleInjectorConfig.Resolver.Create<WordInspectorActor>(), "WordInspectorActor");
+
+            wordInspectorActor.Tell(Messages.SystemCommands.Start, null);
+
+
+            Console.WriteLine("Welcome from AkkaNet.SimpleInjector!! Please enter a word to validate it.");
+            Console.WriteLine("Enter 'exit' to close the program.");
+
+            actorSystem.WhenTerminated.Wait();
         }
     }
 }
--- AkkaNet.SimpleInjector/SimpleInjectorConfig.cs
+++ AkkaNet.SimpleInjector/SimpleInjectorConfig.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Akka.DI.Core;
+using Akka.DI.SimpleInjector;
+using AkkaNet.Actors;
+using AkkaNet.Services;
+using AkkaNet.Services.Interfaces;
+using SimpleInjector;
 
 namespace AkkaNet.SimpleInjector
 {
     class SimpleInjectorConfig
     {
+        private static readonly Container _container;
+        private static IDependencyResolver _resolver;
+
+        static SimpleInjectorConfig()
+        {
+            _container = new Container();
+        }
+
+        public static IDependencyResolver Resolver
+        {
+            get
+            {
+                if (_resolver == null)
+                {
+                    var actorSystem = AkkaConfig.InspectorActorSystem;
+                    _resolver = new SimpleInjectorDependencyResolver(_container, actorSystem);
+                }
+
+                return _resolver;
+            }
+        }
+
+        public static void RegisterInjections()
+        {
+            _container.Register<IPrinterService, PrinterService>(Lifestyle.Singleton);
+        }
     }
 }
